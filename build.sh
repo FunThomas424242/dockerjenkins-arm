7a3f79e7fb9739c400aa5c859916559fced7882f
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-05-26 18:52:53 +0200

    Ausgabe der aktuellen Architektur und Suche der verfügbaren images

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+docker info -f '{{.OSType}}/{{.Architecture}}'
+#
+docker --config ~/dockertestconfig manifest inspect -v laglinaro/jenkinsdocker:master | jq .[].Platform
+#
+docker --config ./dockertestconfig manifest inspect -v jenkinsci/jenkins:2.61 | jq .[].Platform
+#
 docker build -t "funthomas424242/dockerjenkins-arm" .
 
