3ec5e57b05add725299354919e3dd52111e1315a
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-05-26 18:59:57 +0200

    Ausgabe der aktuellen Architektur und Suche der verfügbaren images

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 docker info -f '{{.OSType}}/{{.Architecture}}'
 #
-docker --config ~/dockertestconfig manifest inspect -v laglinaro/jenkinsdocker:master | jq .[].Platform
+docker --config ./dockertestconfig manifest inspect -v laglinaro/jenkinsdocker:master | jq .[].Platform
 #
 docker --config ./dockertestconfig manifest inspect -v jenkinsci/jenkins:2.61 | jq .[].Platform
 #
